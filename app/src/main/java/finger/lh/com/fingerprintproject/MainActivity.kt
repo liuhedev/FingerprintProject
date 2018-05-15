f4a46394142a9f1b620d5bd7be3baec8653fd005
liuhe <he_liu@xtev.cn>
2018-05-15 11:33:40 +0800

    init project

---
+++ app/src/main/java/finger/lh/com/fingerprintproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package finger.lh.com.fingerprintproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
